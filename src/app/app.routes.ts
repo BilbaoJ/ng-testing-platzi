2d6a39bb29f92ca33a633748e580987e2b90e666
BilbaoJ <jessicabilbao02@gmail.com>
2024-08-23 18:50:37 -0500

    Setup project with pico

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
+import { PicPreviewComponent } from './components/pic-preview/pic-preview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductsComponent
+  },
+  {
+    path: 'pico-preview',
+    component: PicPreviewComponent
+  }
+];
---
+++ src/app/components/pic-preview/pic-preview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pic-preview',
+  standalone: true,
+  imports: [],
+  templateUrl: './pic-preview.component.html',
+  styleUrl: './pic-preview.component.scss'
+})
+export class PicPreviewComponent {
+
+}
