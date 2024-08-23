bb03e66d7f6b88c4c9d97fd8980af22a77984fed
BilbaoJ <jessicabilbao02@gmail.com>
2024-08-23 16:47:31 -0500

    Using mocha report

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -1,22 +1,26 @@
 import { Calculator } from "./calculator";
 
 describe('Test for Calculator', () => {
-  it('#multiply should return the result of the numbers multiplied', () => {
-    const calculator = new Calculator();
-    const rta = calculator.multiply(3,3);
-    expect(rta).toEqual(9);
+  describe('Tests  for multiply', () => {
+    it('#multiply should return the result of the numbers multiplied', () => {
+      const calculator = new Calculator();
+      const rta = calculator.multiply(3,3);
+      expect(rta).toEqual(9);
+    });
   });
 
-  it('#divide should return the result of the numbers divided', () => {
-    const calculator = new Calculator();
-    const rta = calculator.divide(4,1);
-    expect(rta).toEqual(4);
-  });
+  describe('Tests  for divide', () => {
+    it('#divide should return the result of the numbers divided', () => {
+      const calculator = new Calculator();
+      const rta = calculator.divide(4,1);
+      expect(rta).toEqual(4);
+    });
 
-  it('#divide should return null when try divided by 0', () => {
-    const calculator = new Calculator();
-    const rta = calculator.divide(4,0);
-    expect(rta).toEqual(null);
+    it('#divide should return null when try divided by 0', () => {
+      const calculator = new Calculator();
+      const rta = calculator.divide(4,0);
+      expect(rta).toEqual(null);
+    });
   });
 
   it('test matchers', () => {
