34335f73b4737178dc639f7203cb4dc6b037bfc3
BilbaoJ <jessicabilbao02@gmail.com>
2024-08-23 17:12:35 -0500

    Testing services

---
+++ src/app/services/value.service.spec.ts
@@ -0,0 +1,52 @@
+import { ValueService } from './value.service';
+
+fdescribe('ValueService', () => {
+  let service: ValueService;
+
+  beforeEach(() => {
+    service = new ValueService();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('Test for getValue', () => {
+    it('should return "my value"', () => {
+      expect(service.getValue()).toBe('my value');
+    });
+  });
+
+  describe('Test for setValue', () => {
+    it('should change the value', () => {
+      expect(service.getValue()).toBe('my value');
+      service.setValue('change');
+      expect(service.getValue()).toBe('change');
+    });
+  });
+
+  describe('Test for getPromise', () => {
+    it('should return "promise value" from promise with then', (doneFn) => {
+      service.getPromiseValue()
+      .then((value) => {
+        expect(value).toBe('promise value');
+        doneFn();
+      });
+    });
+
+    it('should return "promise value" from promise using async', async () => {
+      const rta = await service.getPromiseValue();
+      expect(rta).toBe('promise value');
+    });
+  });
+
+  describe('Test for getObserverValue', () => {
+    it('should return "observable value"', (doneFn) => {
+      service.getObserverValue()
+    .subscribe((value) => {
+      expect(value).toBe('observable value');
+      doneFn();
+    });
+    })
+  });
+});
