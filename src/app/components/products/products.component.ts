567ee132fa4001b5a80961530ebf28849b54e948
BilbaoJ <jessicabilbao02@gmail.com>
2024-08-23 19:20:19 -0500

    Product service http

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+import { Product } from '../../models/product.model';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [ HttpClientModule ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
+
+  products: Product[] = [];
+
+  constructor(
+    private productsService: ProductsService
+  ){}
+
+  ngOnInit(){
+    this.getAllProducts();
+  }
+
+  getAllProducts(){
+    this.productsService.getAllSimple()
+    .subscribe(products => {
+      this.products = products;
+    });
+  }
 
 }
