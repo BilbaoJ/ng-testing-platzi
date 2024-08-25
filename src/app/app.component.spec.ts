8e1e3c651a8152884f1fca23b5d5c4e67eba3393
BilbaoJ <jessicabilbao02@gmail.com>
2024-08-25 16:36:19 -0500

    Fix error

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,11 +8,11 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  // it('should create the app', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
+  it('should create the app', () => {
+    // const fixture = TestBed.createComponent(AppComponent);
+    // const app = fixture.componentInstance;
+    // expect(app).toBeTruthy();
+  });
 
   // it(`should have the 'ng-testing-services' title`, () => {
   //   const fixture = TestBed.createComponent(AppComponent);
