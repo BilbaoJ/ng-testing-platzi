affa4caebd81227ee6e2d749c239248700d125c8
BilbaoJ <jessicabilbao02@gmail.com>
2024-08-28 15:38:26 -0500

    Testing componet with @Input

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PersonComponent } from './person.component';
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import { Person } from '../../models/person.model';
 
 fdescribe('PersonComponent', () => {
   let component: PersonComponent;
@@ -23,17 +24,28 @@ fdescribe('PersonComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have <p> with "Soy un parrafo"', () => {
+  it('should the name be "Pepito"', () => {
+    component.person = new Person('Pepito', 'Perez', 22, 60, 1.70);
+    expect(component.person.name).toEqual('Pepito');
+  });
+
+  it('should have <p> with "Mi altura es {{person.heigth}}"', () => {
+    component.person = new Person('Juan', 'Perez', 22, 58, 1.60);
+    const expectMsg = `Mi altura es ${component.person.heigth}`
     const personDebug: DebugElement = fixture.debugElement;
     const pDebug: DebugElement = personDebug.query(By.css('p'));
     const pElement: HTMLElement = pDebug.nativeElement;
-    expect(pElement?.textContent).toEqual('Soy un parrafo');
+    fixture.detectChanges();
+    expect(pElement?.textContent).toEqual(expectMsg);
   });
 
-  it('should have <h3> with "Hola, PersonComponent"', () => {
+  it('should have <h3> with "Hola, {{person.name}}"', () => {
+    component.person = new Person('Juan', 'Perez', 22, 60, 1.70);
+    const expectMsg = `Hola, ${component.person.name}`
     const personDebug: DebugElement = fixture.debugElement;
     const h3Debug: DebugElement = personDebug.query(By.css('h3'));
     const h3Element: HTMLElement = h3Debug.nativeElement;
-    expect(h3Element?.textContent).toEqual('Hola, PersonComponent');
+    fixture.detectChanges();
+    expect(h3Element?.textContent).toEqual(expectMsg);
   });
 });
