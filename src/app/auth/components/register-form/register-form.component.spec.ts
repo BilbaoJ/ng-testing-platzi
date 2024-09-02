3e99910c38d3a8c9599cc7346e36828f24d8a185
BilbaoJ <jessicabilbao02@gmail.com>
2024-09-02 14:29:52 -0500

    Testing form fields

--- src/app/auth/components/register-form/register-form.component.spec.ts
+++ src/app/auth/components/register-form/register-form.component.spec.ts
@@ -27,4 +27,37 @@ fdescribe('RegisterFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should the email field be invalid', () => {
+    component.emailField?.setValue('esto no es un correo');
+    expect(component.emailField?.invalid).withContext('wrong email').toBeTruthy();
+
+    component.emailField?.setValue('');
+    expect(component.emailField?.invalid).withContext('empty email').toBeTruthy();
+  });
+
+  it('should the passwod field be invalid', () => {
+    component.passwordField?.setValue('');
+    expect(component.passwordField?.invalid).withContext('empty').toBeTruthy();
+
+    component.passwordField?.setValue('12345');
+    expect(component.passwordField?.invalid).withContext('just 5 caracters').toBeTruthy();
+
+    component.passwordField?.setValue('asdasdsadaa');
+    expect(component.passwordField?.invalid).withContext('without numbers').toBeTruthy();
+
+    component.passwordField?.setValue('1asdasdsadaa');
+    expect(component.passwordField?.valid).withContext('rigth').toBeTruthy();
+  });
+
+  it('should the form be invalid', () => {
+    component.form.patchValue({
+      name: 'Pepito',
+      email: 'pepito@mail.com',
+      password: '123456',
+      confirmPassword: '123456',
+      checkTerms: false
+    });
+    expect(component.form.invalid).toBeTruthy();
+  });
 });
