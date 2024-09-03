74987e59c0076bf370d84f9aa4bcefdcb46d22b8
BilbaoJ <jessicabilbao02@gmail.com>
2024-09-03 17:10:17 -0500

    Config product detail

--- src/app/products/components/product/product.component.ts
+++ src/app/products/components/product/product.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../../models/product.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
